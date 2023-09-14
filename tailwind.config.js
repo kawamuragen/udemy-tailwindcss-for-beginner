31296901de9274c7e419eea797acdfcd5cb539ba
kawamuragen <genn.kawamura@gmail.com>
2023-09-14 14:29:41 +0900

    💄 Configure global background-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: "#273444",
+      },
+    },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
